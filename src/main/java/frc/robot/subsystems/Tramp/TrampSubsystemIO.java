4053c39c19bd73d0a1ea04cdb53b6af855d41787
anvisingh12 <anvi3rcs@gmail.com>
2024-06-04 20:25:17 -0700

    more work on the tramp

--- src/main/java/frc/robot/subsystems/Tramp/TrampSubsystemIO.java
+++ src/main/java/frc/robot/subsystems/Tramp/TrampSubsystemIO.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems;
+package frc.robot.subsystems.Tramp;
 
 import org.littletonrobotics.junction.AutoLog;
 
--- src/main/java/frc/robot/subsystems/Tramp/TrampSubsystemIO_HW.java
+++ src/main/java/frc/robot/subsystems/Tramp/TrampSubsystemIO_HW.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems;
+package frc.robot.subsystems.Tramp;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -16,21 +16,43 @@ public class TrampSubsystemIO_HW implements TrampSubsystemIO {
     elevator_motor_ = new TalonFX(6);
     manipulator_motor_ = new CANSparkFlex(9, null);
 
-
+// Setting all the PID values: 
     var arm_pids = new Slot0Configs();
-    arm_pids.kS = 0.0; // Add 0.05 V output to overcome static friction
-    arm_pids.kV = 0.0; // A velocity target of 1 rps results in 0.12 V output
-    arm_pids.kP = 0.0; // An error of 1 rps results in 0.11 V output
+    arm_pids.kS = 0.0; // Add 0.00 V output to overcome static friction
+    arm_pids.kV = 0.0; // A velocity target of 0 rps results in 0.00 V output
+    arm_pids.kP = 0.0; // An error of 0 rps results in 0.0 V output
     arm_pids.kI = 0.0; // no output for integrated error
     arm_pids.kD = 0.0; // no output for error derivative
 
+    var climber_pids = new Slot0Configs();
+    climber_pids.kS = 0.0; // Add 0.00 V output to overcome static friction
+    climber_pids.kV = 0.0; // A velocity target of 0 rps results in 0.00 V output
+    climber_pids.kP = 0.0; // An error of 0 rps results in 0.0 V output
+    climber_pids.kI = 0.0; // no output for integrated error
+    climber_pids.kD = 0.0; // no output for error derivative
+
+    var elevator_pids = new Slot0Configs();
+    elevator_pids.kS = 0.0; // Add 0.00 V output to overcome static friction
+    elevator_pids.kV = 0.0; // A velocity target of 0 rps results in 0.00 V output
+    elevator_pids.kP = 0.0; // An error of 0 rps results in 0.0 V output
+    elevator_pids.kI = 0.0; // no output for integrated error
+    elevator_pids.kD = 0.0; // no output for error derivative
+
+    var manipulator_pids = new Slot0Configs();
+    manipulator_pids.kS = 0.0; // Add 0.00 V output to overcome static friction
+    manipulator_pids.kV = 0.0; // A velocity target of 0 rps results in 0.00 V output
+    manipulator_pids.kP = 0.0; // An error of 0 rps results in 0.0 V output
+    manipulator_pids.kI = 0.0; // no output for integrated error
+    manipulator_pids.kD = 0.0; // no output for error derivative
+
+    elevator_motor_.getConfigurator().apply(elevator_pids);
     arm_motor_.getConfigurator().apply(arm_pids);
+    climber_motor_.getConfigurator().apply(climber_pids);
+    // PIDS for the manipulator motor (SparkFlex) don't work the same way
+    // manipulator_motor_.getConfigurator().apply(manipulator_pids);
     }
 
-// motionmagicvoltage
-
-
-    // actual updateInputs function; updates all of the inputs from ShooterSubsystemIO 
+    // updateInputs function; updates all of the inputs from TrampSubsystemIO 
     @Override
     public void updateInputs(TrampSubsystemIOInputs inputs) {
         inputs.elevatorPosition = elevator_motor_.getPosition().getValueAsDouble(); 
@@ -51,6 +73,8 @@ public class TrampSubsystemIO_HW implements TrampSubsystemIO {
         inputs.climberVoltage = climber_motor_.getMotorVoltage().getValueAsDouble(); 
         inputs.climberAcceleration = climber_motor_.getAcceleration().getValueAsDouble(); 
 
+        // Inputs for the manipulator motor (SparkFlex) also work differently: 
+        //
         // inputs.manipulatorPosition = manipulator_motor_.getPosition().getValueAsDouble(); 
         // inputs.manipulatorVelocity = manipulator_motor_.getVelocity().getValueAsDouble(); 
         // inputs.manipulatorCurrent = manipulator_motor_.getSupplyCurrent().getValueAsDouble(); 
@@ -73,10 +97,6 @@ public class TrampSubsystemIO_HW implements TrampSubsystemIO {
     public CANSparkFlex getManipulator(){
         return manipulator_motor_;
     }     
-
-
-
-    
 }
 
 // irrelevant code, from the shooter roller assignment: 
