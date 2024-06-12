e12f098b9c4bcbcacd8f575c5b989e7ab8b29447
BlazeGamesYT <bryceblazegaming@gmail.com>
2024-06-11 22:11:33 -0700

    fixed

--- src/main/java/frc/robot/subsystems/Limelight/LimelightIO.java
+++ src/main/java/frc/robot/subsystems/Limelight/LimelightIO.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.Limelight;
 
 public interface LimelightIO {
-    public abstract void setLed(int mode);
+    public abstract void forceOff();
+    public abstract void forceBlink();
+    public abstract void forceOn();
 }
