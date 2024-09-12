48172e509ab9859a6fdd2384c865187034bc0910
NAMAN1034 <155015788+NAMAN1034@users.noreply.github.com>
2024-09-11 21:13:07 -0700

    Create XeroSubsystem.java

---
+++ src/main/java/frc/org/xero1425/XeroSubsystem.java
@@ -0,0 +1,35 @@
+package frc.org.xero1425;
+
+import java.util.List;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.CANSparkBase;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public abstract class XeroSubsystem extends SubsystemBase {
+    private XeroRobot robot_ ;
+    private int logger_id_ ;
+
+    public XeroSubsystem(XeroRobot robot, String name) {
+        super(name) ;
+
+        robot_ = robot ;
+        logger_id_ = getMessageLogger().registerSubsystem(name) ;
+    }
+
+    public XeroRobot getRobot() {
+        return robot_ ;
+    }
+
+    protected MessageLogger getMessageLogger() {
+        return robot_.getMessageLogger() ;
+    }
+
+    public int getMessageLoggerID() {
+        return logger_id_ ;
+    }
+
+    public abstract List<TalonFX> getCTREMotors() ;
+    public abstract List<CANSparkBase> getRevRoboticsMotors() ;
+}
