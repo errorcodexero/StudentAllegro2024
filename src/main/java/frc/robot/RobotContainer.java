ecec1854eb8c95e6bd3231d7abfc67b5bced7699
BlazeDev <73793525+blaze-developer@users.noreply.github.com>
2024-09-16 21:33:05 +0000

    change deadband

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -86,9 +86,9 @@ public class RobotContainer {
     private void setupDrivetrain() {
         drivetrain_.setDefaultCommand( // Drivetrain will execute this command periodically
             drivetrain_.applyRequest(() -> drive_
-                .withVelocityX(Math.pow(MathUtil.applyDeadband(-gamepad_.getLeftY(), 0.05), 2) * maxSpeed_) // Drive forward with negative Y (forward)
-                .withVelocityY(Math.pow(MathUtil.applyDeadband(-gamepad_.getLeftX(), 0.05), 2) * maxSpeed_) // Drive left with negative X (left)
-                .withRotationalRate(Math.pow(MathUtil.applyDeadband(-gamepad_.getRightX(), 0.05), 2) * maxAngularRate_) // Drive counterclockwise with negative X (left)
+                .withVelocityX(Math.pow(MathUtil.applyDeadband(-gamepad_.getLeftY(), 0.04), 2) * maxSpeed_) // Drive forward with negative Y (forward)
+                .withVelocityY(Math.pow(MathUtil.applyDeadband(-gamepad_.getLeftX(), 0.04), 2) * maxSpeed_) // Drive left with negative X (left)
+                .withRotationalRate(Math.pow(MathUtil.applyDeadband(-gamepad_.getRightX(), 0.04), 2) * maxAngularRate_) // Drive counterclockwise with negative X (left)
             )
         );
     }
