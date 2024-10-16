ed945e4c234ff6f488dbaf12b960105d7e4dbb05
blaze-developer <bryceblazegaming@gmail.com>
2024-10-15 19:42:42 -0700

    Rename TunerConstants to Robot specific name and remove unused telemetry class.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.drive.TeleopSwerveDrive;
-import frc.robot.generated.TunerConstants;
+import frc.robot.generated.CompSwerveConstants;
 import frc.robot.subsystems.IntakeShooter.IntakeShooterIOHardware;
 import frc.robot.subsystems.IntakeShooter.IntakeShooterSubsystem;
 import frc.robot.subsystems.Swerve.SwerveSubsystem;
@@ -32,7 +32,7 @@ public class RobotContainer {
     
     // Subsystems
     
-    private final SwerveSubsystem drivetrain_ = new SwerveSubsystem(TunerConstants.DriveTrain); 
+    private final SwerveSubsystem drivetrain_ = new SwerveSubsystem(CompSwerveConstants.DriveTrain); 
 
     private final IntakeShooterSubsystem intake_shooter_ =
         new IntakeShooterSubsystem(new IntakeShooterIOHardware());
