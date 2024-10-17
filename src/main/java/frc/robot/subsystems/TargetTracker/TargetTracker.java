a60a582e03acd0d0a2f31996c0cf6f64e50d23e1
BlazeDev <73793525+blaze-developer@users.noreply.github.com>
2024-10-17 17:46:36 +0000

    remove imports

--- src/main/java/frc/robot/subsystems/TargetTracker/TargetTracker.java
+++ src/main/java/frc/robot/subsystems/TargetTracker/TargetTracker.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.TargetTracker;
 
-import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.Radians;
 
